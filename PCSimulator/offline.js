371b6e9220f535fb5442c4ca2382a477ad5cbb4a
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2024-09-21 16:01:07 +0300

    Add files via upload

---
+++ PCSimulator/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1726923568,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bin-sheet0.png",
+		"images/file-sheet0.png",
+		"images/bar-sheet0.png",
+		"images/background-sheet0.png",
+		"images/chrome-sheet0.png",
+		"images/background0-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
